b29c33eccac0d52b860c8443e17234eea7ad9f3d
manikandanr734 <manikandanr734@gmail.com>
2025-07-20 07:12:14 +0530

    day1

---
+++ day1/chrome.java
@@ -0,0 +1,23 @@
+package week1.assignment.day1;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		
+		// declaring variables
+		
+				float version = 91.01f;
+				String developer = "Google";
+				boolean isBeta = false;
+				int releaseYear = 2008;
+				char shortcutKey = 'C';
+				
+				System.out.println(version);
+				System.out.println(developer);
+				System.out.println(isBeta);
+				System.out.println(releaseYear);
+				System.out.println(shortcutKey);
+
+	}
+
+}
